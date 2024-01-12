c067e62d9cd6c0fa94c8b02f0c8841168f19c80a
dongkeonkim <dongkeonkim0810@gmail.com>
2024-01-12 21:39:35 +0900

    백준 10989

---
+++ 백준/Bronze/수 정렬하기 3/Main.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    int[] cnt = new int[10001];
+    int N = Integer.parseInt(br.readLine());
+
+    for (int i = 0; i < N; i++) {
+      cnt[Integer.parseInt(br.readLine())]++;
+    }
+
+    br.close();
+
+    StringBuilder sb = new StringBuilder();
+
+    for (int i = 1; i < 10001; i++) {
+      while (cnt[i] > 0) {
+        bw.write(i + "\n");
+        cnt[i]--;
+      }
+    }
+
+    bw.flush();
+    bw.close();
+  }
+}
