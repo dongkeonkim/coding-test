84ef48a613bccdacf152d29efb191c935b75dd88
dongkeonkim <dongkeonkim0810@gmail.com>
2024-02-12 13:00:28 +0900

    백준 8393

---
+++ 백준/Bronze/합 /Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int N = sc.nextInt();
+    sc.close();
+
+    int total = 0;
+
+    for (int i = 1; i <= N; i++) {
+      total += i;
+    }
+    System.out.println(total);
+  }
+}
