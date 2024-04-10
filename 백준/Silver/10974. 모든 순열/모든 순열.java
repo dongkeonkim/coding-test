62d0908c1a882a57aecbaf491b5282212b822955
Dong-keon Kim <96672917+dongkeonkim@users.noreply.github.com>
2024-04-10 14:36:16 +0900

    [Silver III] Title: 모든 순열, Time: 324 ms, Memory: 34460 KB -BaekjoonHub

---
+++ 백준/Silver/10974. 모든 순열/모든 순열.java
@@ -0,0 +1,45 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static int N;
+    static int[] arr;
+    static boolean[] visited;
+    static StringBuilder sb;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        sb = new StringBuilder();
+
+        N = Integer.parseInt(st.nextToken());
+        arr = new int[N];
+        visited = new boolean[N];
+
+        dfs(0);
+
+        System.out.println(sb);
+    }
+
+    public static void dfs(int depth) {
+        if (depth == N) {
+            for (int temp : arr) {
+                sb.append(temp + " ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for (int i = 0; i < N; i++) {
+            if (!visited[i]) {
+                visited[i] = true;
+                arr[depth] = i + 1;
+                dfs(depth + 1);
+                visited[i] = false;
+            }
+        }
+    }
+}
