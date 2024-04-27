f6a30a3d0ccb2a184a03144538dc5c7ea420b2b9
Dong-keon Kim <96672917+dongkeonkim@users.noreply.github.com>
2024-04-27 22:22:22 +0900

    [Silver II] Title: N과 M (11), Time: 1592 ms, Memory: 334784 KB -BaekjoonHub

---
+++ 백준/Silver/15665. N과 M （11）/N과 M （11）.java
@@ -0,0 +1,64 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.LinkedHashSet;
+import java.util.Set;
+import java.util.StringTokenizer;
+
+public class Main {
+
+  static int N;
+  static int M;
+
+  static Set<String> set;
+
+  static int[] arr;
+  static int[] inputs;
+
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st = new StringTokenizer(br.readLine());
+
+    N = Integer.parseInt(st.nextToken());
+    M = Integer.parseInt(st.nextToken());
+
+    arr = new int[M];
+    inputs = new int[N];
+
+    st = new StringTokenizer(br.readLine());
+    for (int i = 0; i < N; i++) {
+      inputs[i] = Integer.parseInt(st.nextToken());
+    }
+
+    Arrays.sort(inputs);
+
+    set = new LinkedHashSet<>();
+
+    dfs(0);
+
+    StringBuilder sb = new StringBuilder();
+
+    for (String t : set) {
+      sb.append(t);
+    }
+
+    System.out.println(sb);
+  }
+
+  public static void dfs(int depth) {
+    if (depth == M) {
+      String result = "";
+      for (int temp : arr) {
+        result += temp + " ";
+      }
+      set.add(result + "\n");
+      return;
+    }
+
+    for (int i = 0; i < N; i++) {
+      arr[depth] = inputs[i];
+      dfs(depth + 1);
+    }
+  }
+}
