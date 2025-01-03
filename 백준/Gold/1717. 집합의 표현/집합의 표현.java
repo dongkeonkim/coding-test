68094c6de48c729580ce65dc810bf54ddfe0e7a3
DongKeon Kim <96672917+dongkeonkim@users.noreply.github.com>
2025-01-03 23:40:59 +0900

    [Gold V] Title: 집합의 표현, Time: 1160 ms, Memory: 249884 KB -BaekjoonHub

--- 백준/Gold/1717. 집합의 표현/집합의 표현.java
+++ 백준/Gold/1717. 집합의 표현/집합의 표현.java
@@ -1,65 +1,65 @@
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
 
-    static int[] parent;
-    static int[] rank;
+  static int[] parent;
+  static int[] rank;
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int m = sc.nextInt();
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+    int m = sc.nextInt();
 
-        parent = new int[n + 1];
-        rank = new int[n + 1];
-        for (int i = 0; i <= n; i++) {
-            parent[i] = i;
-            rank[i] = 0;
-        }
-
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < m; i++) {
-            int type = sc.nextInt();
-            int a = sc.nextInt();
-            int b = sc.nextInt();
-
-            if (type == 0) {
-                union(a, b);
-            } else {
-                if (find(a) == find(b)) {
-                    sb.append("YES\n");
-                } else {
-                    sb.append("NO\n");
-                }
-            }
-        }
-
-        sc.close();
-        System.out.println(sb);
+    parent = new int[n + 1];
+    rank = new int[n + 1];
+    for (int i = 0; i <= n; i++) {
+      parent[i] = i;
+      rank[i] = 0;
     }
 
-    private static int find(int x) {
-        if (parent[x] != x) {
-            return find(parent[x]);
+    StringBuilder sb = new StringBuilder();
+    for (int i = 0; i < m; i++) {
+      int type = sc.nextInt();
+      int a = sc.nextInt();
+      int b = sc.nextInt();
+
+      if (type == 0) {
+        union(a, b);
+      } else {
+        if (find(a) == find(b)) {
+          sb.append("YES\n");
         } else {
-            return parent[x];
+          sb.append("NO\n");
         }
+      }
     }
 
-    private static void union(int a, int b) {
-        int rootA = find(a);
-        int rootB = find(b);
+    sc.close();
 
-        if (rootA != rootB) {
-            if (rank[rootA] > rank[rootB]) {
-                parent[rootB] = rootA;
-            } else if (rank[rootA] < rank[rootB]) {
-                parent[rootA] = rootB;
-            } else {
-                parent[rootB] = rootA;
-                rank[rootA]++;
-            }
-        }
+    System.out.println(sb);
+  }
+
+  private static int find(int x) {
+    if (parent[x] == x) {
+      return x;
+    } else {
+      return parent[x] = find(parent[x]);
     }
+  }
 
+  private static void union(int x, int y) {
+    int rootX = find(x);
+    int rootY = find(y);
+
+    if (rootX != rootY) {
+      if (rank[rootX] > rank[rootY]) {
+        parent[rootY] = rootX;
+      } else if (rank[rootY] < rank[rootX]) {
+        parent[rootX] = rootY;
+      } else {
+        parent[rootX] = rootY;
+        rank[rootY]++;
+      }
+    }
+  }
 }
