a2009b2226a8ffb49915bb3ee194555f22229219
DongKeon Kim <96672917+dongkeonkim@users.noreply.github.com>
2025-01-03 23:56:15 +0900

    [Silver II] Title: 특정 거리의 도시 찾기, Time: 2680 ms, Memory: 365948 KB -BaekjoonHub

--- 백준/Silver/18352. 특정 거리의 도시 찾기/특정 거리의 도시 찾기.java
+++ 백준/Silver/18352. 특정 거리의 도시 찾기/특정 거리의 도시 찾기.java
@@ -1,75 +1,64 @@
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
 
-  public static void main(String[] args) {
-    Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int M = sc.nextInt();
+        int K = sc.nextInt();
+        int X = sc.nextInt();
 
-    int N = sc.nextInt();
-    int M = sc.nextInt();
-    int K = sc.nextInt();
-    int X = sc.nextInt();
+        ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
 
-    ArrayList<ArrayList<Integer>> list = new ArrayList<>();
-    for (int i = 0; i <= N; i++) {
-      list.add(new ArrayList<Integer>());
-    }
-
-    for (int i = 0; i < M; i++) {
-      int from = sc.nextInt();
-      int to = sc.nextInt();
-      list.get(from).add(to);
-    }
-
-    sc.close();
-
-    ArrayList<Integer> result = bfs(list, N, K, X);
-
-    Collections.sort(result);
-    if (result.isEmpty()) {
-      System.out.println(-1);
-    } else {
-      for (int i : result) {
-        System.out.println(i);
-      }
-    }
-  }
-
-  private static ArrayList<Integer> bfs(
-    ArrayList<ArrayList<Integer>> graph,
-    int N,
-    int K,
-    int X
-  ) {
-    ArrayList<Integer> result = new ArrayList<>();
-    boolean[] visited = new boolean[N + 1];
-    Queue<Integer[]> queue = new LinkedList<>();
-    visited[X] = true;
-    queue.add(new Integer[] { X, 0 });
-
-    while (!queue.isEmpty()) {
-      Integer[] current = queue.poll();
-      int city = current[0];
-      int cnt = current[1];
-
-      if (cnt == K) {
-        result.add(city);
-        continue;
-      }
-
-      for (int nextCity : graph.get(city)) {
-        if (!visited[nextCity]) {
-          visited[nextCity] = true;
-          queue.add(new Integer[] { nextCity, cnt + 1 });
+        for (int i = 0; i <= N; i++) {
+            graph.add(new ArrayList<>());
         }
-      }
+
+        for (int i = 0; i < M; i++) {
+            int from = sc.nextInt();
+            int to = sc.nextInt();
+            graph.get(from).add(to);
+        }
+        sc.close();
+
+        ArrayList<Integer> result = bfs(graph, X, K);
+        Collections.sort(result);
+
+        if (result.isEmpty()) {
+            System.out.println("-1");
+        } else {
+            for (int i : result) {
+                System.out.println(i);
+            }
+        }
     }
 
-    return result;
-  }
+    public static ArrayList<Integer> bfs(ArrayList<ArrayList<Integer>> graph, int X, int K) {
+        Queue<int[]> queue = new LinkedList<>();
+        boolean[] visited = new boolean[graph.size() + 1];
+        ArrayList<Integer> result = new ArrayList<>();
+
+        queue.add(new int[] { X, 0 });
+        visited[X] = true;
+
+        while (!queue.isEmpty()) {
+            int[] current = queue.poll();
+            int city = current[0];
+            int distance = current[1];
+
+            if (distance == K) {
+                result.add(city);
+                continue;
+            }
+
+            for (int neighbor : graph.get(city)) {
+                if (!visited[neighbor]) {
+                    queue.add(new int[] { neighbor, distance + 1 });
+                    visited[neighbor] = true;
+                }
+            }
+        }
+        return result;
+    }
 }
