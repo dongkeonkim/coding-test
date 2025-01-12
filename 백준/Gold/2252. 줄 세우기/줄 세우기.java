0773dc1717b5d918a70746e4869ba50c86dd86da
DongKeon Kim <96672917+dongkeonkim@users.noreply.github.com>
2025-01-12 16:55:45 +0900

    [Gold III] Title: 줄 세우기, Time: 1248 ms, Memory: 176628 KB -BaekjoonHub

--- 백준/Gold/2252. 줄 세우기/줄 세우기.java
+++ 백준/Gold/2252. 줄 세우기/줄 세우기.java
@@ -1,55 +1,70 @@
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        
-        int N = sc.nextInt();
-        int M = sc.nextInt();
-        
-        List<List<Integer>> graph = new ArrayList<>();
-        int[] inDegree = new int[N + 1];
-        
-        for (int i = 0; i <= N; i++) {
-            graph.add(new ArrayList<>());
-        }
-        
-        for (int i = 0; i < M; i++) {
-            int A = sc.nextInt();
-            int B = sc.nextInt();
-            graph.get(A).add(B);
-            inDegree[B]++;
-        }
-        
-        List<Integer> result = topologicalSort(graph, inDegree, N);
-        
-        for (int student : result) {
-            System.out.print(student + " ");
-        }
+
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int N = sc.nextInt();
+    int M = sc.nextInt();
+
+    Node[] nodes = new Node[N + 1];
+    for (int i = 1; i <= N; i++) {
+      nodes[i] = new Node(i);
     }
 
-    private static List<Integer> topologicalSort(List<List<Integer>> graph, int[] inDegree, int N) {
-        Queue<Integer> queue = new LinkedList<>();
-        List<Integer> result = new ArrayList<>();
-        
-        for (int i = 1; i <= N; i++) {
-            if (inDegree[i] == 0) {
-                queue.add(i);
-            }
-        }
-        
-        while (!queue.isEmpty()) {
-            int current = queue.poll();
-            result.add(current);
-            
-            for (int neighbor : graph.get(current)) {
-                inDegree[neighbor]--;
-                if (inDegree[neighbor] == 0) {
-                    queue.add(neighbor);
-                }
-            }
-        }
-        
-        return result;
+    for (int i = 0; i < M; i++) {
+      int a = sc.nextInt();
+      int b = sc.nextInt();
+      nodes[a].neighbors.add(b);
+      nodes[b].inDegree++;
     }
+
+    sc.close();
+
+    List<Integer> results = customSort(nodes);
+    for (int result : results) {
+      System.out.print(result + " ");
+    }
+  }
+
+  private static List<Integer> customSort(Node[] nodes) {
+    List<Integer> results = new ArrayList<>();
+    Queue<Node> queue = new LinkedList<>();
+
+    for (int i = 1; i < nodes.length; i++) {
+      if (nodes[i].inDegree == 0) {
+        queue.add(nodes[i]);
+      }
+    }
+
+    while (!queue.isEmpty()) {
+      Node node = queue.poll();
+
+      if (node.inDegree == 0) {
+        results.add(node.id);
+      }
+
+      for (int neighbor : node.neighbors) {
+        nodes[neighbor].inDegree--;
+        if (nodes[neighbor].inDegree == 0) {
+          queue.add(nodes[neighbor]);
+        }
+      }
+    }
+
+    return results;
+  }
+}
+
+class Node {
+
+  int id;
+  int inDegree;
+  List<Integer> neighbors;
+
+  public Node(int id) {
+    this.id = id;
+    this.inDegree = 0;
+    this.neighbors = new ArrayList<>();
+  }
 }
